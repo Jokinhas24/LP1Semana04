cc87017346520ea9f54bcfe6fca64eea1c24d1ce
João Nogueira <joaonogueira.art@gmail.com>
2024-03-15 00:02:26 +0000

    Added code to get the special number in the sequence

---
+++ Special/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.XPath;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            int result = 0;
+
+            if (n == 0)
+            {
+                result = 0;
+            }
+            else if (n == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            string ns = Console.ReadLine();
+            int n = int.Parse(ns);
+
+            Console.Write(GetSpecial(n));
+        }
+    }
+}
