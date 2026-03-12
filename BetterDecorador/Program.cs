973a39edda5bde8c9359d5da6bb31c7cfe327cfd
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-12 00:18:52 +0000

    now works with the number

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+        }
+
+        private static string Decor(string s, char dec, int it)
+        {
+            string decorators = new string(dec, it);
+            return $"{decorators} {s} {decorators}";
+        }
+    }
+}
