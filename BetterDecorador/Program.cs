508bc6b173ee0b0643aae7a8ff86ec70d57b388b
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-12 00:31:28 +0000

    Add new message when no arguments are given

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -5,24 +5,42 @@ namespace BetterDecorador
     public class Program
     {
         /// <summary>
-        /// This method calls Decor with arguments given in the console
+        /// This method calls Decor with arguments given in the console,
+        /// if there are none it calls it without them.
         /// </summary>
-        /// <param name="args"> arguments used to call the decorator </param>
+        /// <param name="args"> Arguments used to call the decorator. </param>
         private static void Main(string[] args)
         {
-            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+            if (args.Length == 0)
+            {
+                Console.WriteLine(Decor());
+            }
+            else
+            {
+                Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+            }
         }
+
         /// <summary>
-        /// This method decorates a string with a char a specific number of times around the string
+        /// This method decorates a string with a char a specific number of times around the string.
         /// </summary>
-        /// <param name="s"> decorated string </param>
-        /// <param name="dec"> char that decorates</param>
-        /// <param name="it"> how many times the char is repeated </param>
+        /// <param name="s"> Decorated string. </param>
+        /// <param name="dec"> Char that decorates. </param>
+        /// <param name="it"> How many times the char is repeated. </param>
         /// <returns></returns>
         private static string Decor(string s, char dec, int it)
         {
             string decorators = new string(dec, it);
             return $"{decorators} {s} {decorators}";
+        }
+
+        /// <summary>
+        /// This method is called when there are no arguments.
+        /// </summary>
+        /// <returns> A warning message using the decorator. </returns>
+        private static string Decor()
+        {
+            return Decor("User did not specify args!", '=', 3)
         }
     }
 }
