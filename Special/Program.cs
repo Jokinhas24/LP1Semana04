912a4c573edf9b25cc3461feeea584d31efefaa1
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-12 00:51:10 +0000

    Special does the thing

---
+++ Special/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int numb = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(numb));
+        }
+        private static int GetSpecial(int n)
+        {
+            int spnumb;
+            if (n == 0)
+            {
+                spnumb = 0;
+            }
+            else if (n == 1)
+            {
+                spnumb = 1;
+            }
+            else
+            {
+                spnumb = GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+            }
+            return spnumb;
+        }
+    }
+}
